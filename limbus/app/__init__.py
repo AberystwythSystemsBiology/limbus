668aeb1a2eb6be6e92ee585423647181b7a5d922
KeironO <keiron.oshea@gmail.com>
2019-09-28 12:41:33 +0100

    [MISC] Added yarn to gitignore

--- limbus/app/__init__.py
+++ limbus/app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 
 from config import app_config
 
+# blueprint imports
+from .misc import misc as misc_blueprint
+
+
 def create_app(flask_config):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[flask_config])
     app.config.from_pyfile("config.py")
 
-    from .misc import misc as misc_blueprint
     app.register_blueprint(misc_blueprint)
 
     return app
