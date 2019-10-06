4a125f35cd90ea25955c9859b549f094619299ce
KeironO <keiron.oshea@gmail.com>
2019-10-06 11:56:29 +0100

    [DEVOPS] Podman combat

--- bash2fish.py
+++ bash2fish.py
@@ -5,7 +5,6 @@ fish = ""
 with open("helpers.sh", "r") as infile:
     for line in infile.readlines():
         line = re.sub("/bin/bash", "/bin/fish", line)
-
         line = re.sub("[{()]", "", line)
         line = re.sub("[}]", "end", line)
         fish += "%s" % (line)
