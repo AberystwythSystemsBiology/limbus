5083f3a664d0df3999dda764042c5c3ca2e3ba6d
KeironO <keiron.oshea@gmail.com>
2019-12-12 16:03:51 +0000

    [IMP] Added more SAT

--- limbus/app/sample/views.py
+++ limbus/app/sample/views.py
@@ -20,9 +20,11 @@ def index():
 def add_sample():
     return "Hello World"
 
+# Attribute Stuff
+
 @sample.route("attribute/")
 def attribute_portal():
-    sample_attributes = db.session.query(SampleAttribute).all()
+    sample_attributes = db.session.query(SampleAttribute, User).filter(SampleAttribute.author_id == User.id).all()
 
     return render_template("sample/attribute/index.html", sample_attributes=sample_attributes)
 
@@ -41,3 +43,10 @@ def add_attribute():
 
         return redirect(url_for("sample.index"))
     return render_template("sample/attribute/add.html", form=form)
+
+@sample.route("attribute/view/<attribute_id>")
+def view_attribute(attribute_id):
+    attribute, attribute_user = db.session.query(SampleAttribute, User).filter(
+        SampleAttribute.id == attribute_id
+    ).filter(SampleAttribute.author_id == User.id).first()
+    return render_template("sample/attribute/view.html", attribute=attribute, attribute_user=attribute_user)
