00830ad88d94127cc5f1efb86e2ac343fcc6d307
KeironO <keiron.oshea@gmail.com>
2020-02-02 18:06:22 +0000

    [IMP] Added lots of stuff

--- limbus/app/demo/views.py
+++ limbus/app/demo/views.py
@@ -1,7 +1,7 @@
 from . import demo
 from .. import db
 
-from ..sample.models import Sample
+from ..sample.models import Sample, SampleAttribute, SampleAttributeTextSetting, SampleAttributeOption
 from ..auth.models import User
 from random import randrange, choice
 import datetime
@@ -32,28 +32,61 @@ def _generate_user_info():
 
 
 def _generate_sample_data() -> None:
-
     users = db.session.query(User).all()
-
-    with open("/limbus/demo-data/sample.csv", "r") as infile:
-        csv_reader = list(csv.reader(infile, delimiter=","))
-
-        for row in csv_reader[1:]:
-            sample = Sample(
-                sample_type=row[0],
-                sample_status=row[3],
-                collection_date=_generate_time("before"),
-                disposal_instruction=row[1],
-                batch_number=row[2],
-                disposal_date=_generate_time("after"),
-                author_id=choice(users).id
+    for row in read_csv("/limbus/demo-data/sample.csv"):
+        sample = Sample(
+            sample_type=row[0],
+            sample_status=row[3],
+            collection_date=_generate_time("before"),
+            disposal_instruction=row[1],
+            batch_number=row[2],
+            disposal_date=_generate_time("after"),
+            author_id=choice(users).id
             )
 
-            db.session.add(sample)
-            db.session.commit()
+        db.session.add(sample)
+        db.session.commit()
+
+def _generate_sample_attribute() -> None:
+    users = db.session.query(User).all()
+    for row in read_csv("/limbus/demo-data/sampleattribute.csv"):
+        aid = choice(users).id
+        sampleattr = SampleAttribute(
+            term = row[0],
+            type = row[1],
+            required = False,
+            author_id= aid
+        )
+
+        db.session.add(sampleattr)
+        db.session.flush()
+
+        if row[1] == "TEXT":
+            setting = SampleAttributeTextSetting(
+                max_length = row[2],
+                sample_attribute_id=sampleattr.id
+            )
+
+            db.session.add(setting)
+
+        if row[1] == "OPTION":
+            for option in row[2].split(";"):
+                attropt = SampleAttributeOption(
+                    term = option,
+                    author_id = aid,
+                    sample_attribute_id=sampleattr.id
+                )
+
+                db.session.add(attropt)
+
+        db.session.commit()
+
+def _associate_sample_attributes() -> None:
+    pass
 
 @demo.route("/", methods=["GET", "POST"])
 def insert_data():
     #_generate_user_info()
-    _generate_sample_data()
+    #_generate_sample_data()
+    #_generate_sample_attribute()
     return _generate_time()
