b7d10cc72a41d9774b63fd559e3e19a24c397ceb
KeironO <keiron.oshea@gmail.com>
2020-02-26 21:29:06 +0000

    [I] Progress

--- limbus/app/processing/views/processing.py
+++ limbus/app/processing/views/processing.py
@@ -6,6 +6,9 @@ from .. import processing
 
 from ..forms import NewProtocolForm, FluidCheckList
 
+from ...misc.generators import generate_random_hash
+
+
 @processing.route("/protocols")
 def protocol_index():
     return render_template("processing/protocols/index.html")
@@ -14,8 +17,33 @@ def protocol_index():
 @processing.route("/protocols/new", methods=["GET", "POST"])
 def new_protocol():
     form = NewProtocolForm()
+    if form.validate_on_submit():
+        protocol_hash = generate_random_hash()
+
+        session["%s protocol_information" % (protocol_hash)] = {
+            "name": form.name.data,
+            "type": form.type.data
+        }
+
+        return redirect(url_for('processing.new_protocol_two', hash=protocol_hash))
+
     return render_template("processing/protocols/new/one.html", form=form)
 
 @processing.route("/protocols/new/two/<hash>")
 def new_protocol_two(hash):
-    pass
+
+    if session["%s protocol_information" % (hash)]["type"] == "FLU":
+        form = FluidCheckList()
+
+        if form.validate_on_submit():
+            session["%s fluid_steps"] = {
+                "pre-cent": form.
+            }
+
+    else:
+        return abort(501)
+
+    return render_template("processing/protocols/new/two.html", form=form)
+    
+
+    
