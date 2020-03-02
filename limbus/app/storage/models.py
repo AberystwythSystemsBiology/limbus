c2087f7ff4a6836ea69fa80cb4ab8933df5ff3a7
KeironO <keiron.oshea@gmail.com>
2020-03-02 22:24:15 +0000

    [I] Added loads of SS stuff

--- limbus/app/storage/models.py
+++ limbus/app/storage/models.py
@@ -37,3 +37,45 @@ class Room(db.Model):
     )
 
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+class ColdStorage(db.Model):
+    __tablename__ = "cold_storage"
+
+    id = db.Column(db.Integer, primary_key=True)
+    serial_number = db.Column(db.String)
+    manufacturer = db.Column(db.String)
+
+    temperature = db.Column(db.String)
+    num_shelves = db.Column(db.Integer, nullable=False)
+    # Enumerated type for freezer/fridge.
+    #type = db.Column(db.Enum())
+
+    creation_date = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+
+    site_id = db.Column(db.Integer, db.ForeignKey("sites.id"))
+
+    update_date = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        server_onupdate=db.func.now(),
+        nullable=False,
+    )
+
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+class DataPaqRack(db.Model):
+    __tablename__ = "data_paq_rack"
+    id = db.Column(db.Integer, primary_key=True)
+
+    pass
+
+    creation_date = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+
+    update_date = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        server_onupdate=db.func.now(),
+        nullable=False,
+    )
+
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
