f61c829870054392d30c6b05bcbee4dccef9a408
KeironO <keiron.oshea@gmail.com>
2020-03-04 12:29:05 +0000

    [I] All of the storage BE

--- limbus/app/storage/models.py
+++ limbus/app/storage/models.py
@@ -1,4 +1,5 @@
 from app import db
+from .enums import *
 
 class Site(db.Model):
     __tablename__ = "sites"
@@ -38,17 +39,17 @@ class Room(db.Model):
 
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
-class ColdStorage(db.Model):
-    __tablename__ = "cold_storage"
+class FixedColdStorage(db.Model):
+    __tablename__ = "fixed_cold_storage"
 
     id = db.Column(db.Integer, primary_key=True)
     serial_number = db.Column(db.String)
     manufacturer = db.Column(db.String)
 
-    temperature = db.Column(db.String)
+    temperature = db.Column(db.Enum(FixedColdStorageTemps))
     num_shelves = db.Column(db.Integer, nullable=False)
-    # Enumerated type for freezer/fridge.
-    #type = db.Column(db.Enum())
+
+    type = db.Column(db.Enum(FixedColdStorageType))
 
     creation_date = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
 
@@ -63,13 +64,54 @@ class ColdStorage(db.Model):
 
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
-class DataPaqRack(db.Model):
-    __tablename__ = "data_paq_rack"
+class DeviceManualToFixedColdStorage(db.Model):
+    __tablename__ = "device_manual_to_fixed_cold_storage"
     id = db.Column(db.Integer, primary_key=True)
 
-    pass
+    storage_id = db.Column(db.Integer, db.ForeignKey("fixed_cold_storage.id"))
+    document_id = db.Column(db.Integer, db.ForeignKey("documents.id"))
 
     creation_date = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+
+    update_date = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        server_onupdate=db.func.now(),
+        nullable=False,
+    )
+
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+class SampleToFixedColdStorage():
+    __tablename__ = "sample_to_fixed_cold_storage"
+    id = db.Column(db.Integer, primary_key=True)
+
+    sample_id = db.Column(db.Integer, db.ForeignKey("samples.id"))
+    storage_id = db.Column(db.Integer, db.ForeignKey("fixed_cold_storage.id"))
+
+    creation_date = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+
+    update_date = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        server_onupdate=db.func.now(),
+        nullable=False,
+    )
+
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+class CryovialBox(db.Model):
+    __tablename__ = "cryovial_boxes"
+    id = db.Column(db.Integer, primary_key=True)
+
+    serial = db.Column(db.String)
+
+    creation_date = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+
+    num_rows = db.Column(db.Integer)
+    num_cols = db.Column(db.Integer)
+
+    data = db.Column(db.JSON)
 
     update_date = db.Column(
         db.DateTime,
