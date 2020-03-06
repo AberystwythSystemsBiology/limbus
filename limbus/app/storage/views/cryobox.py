25b92a42392151449416742cc9fe3779b7da38bc
KeironO <keiron.oshea@gmail.com>
2020-03-06 18:19:11 +0000

    [I] More Cryo stuff added

--- limbus/app/storage/views/cryobox.py
+++ limbus/app/storage/views/cryobox.py
@@ -17,6 +17,16 @@ def cryobox_index():
 
 @storage.route("/cryobox/new", methods=["GET", "POST"])
 def add_cryobox():
+
+    def __generate_data(n_cols, n_rows):
+        columns = {"column %i" % (c): None for c in range(n_cols)}
+
+        data = {
+            "row %i" % (r): columns for r in range(n_rows)
+        }
+
+        return data
+
     form = NewCryovialBoxForm()
 
     if form.validate_on_submit():
@@ -25,7 +35,7 @@ def add_cryobox():
             serial=form.serial.data,
             num_rows=form.num_rows.data,
             num_cols=form.num_cols.data,
-            data={},
+            data=__generate_data(form.num_cols.data, form.num_rows.data),
             author_id=current_user.id,
         )
 
@@ -35,3 +45,13 @@ def add_cryobox():
         return redirect(url_for("storage.cryobox_index"))
 
     return render_template("storage/cryobox/new.html", form=form)
+
+@storage.route("/cryobox/view/LIMBCR-<cryo_id>")
+def view_cryobox(cryo_id):
+    cryo = db.session.query(CryovialBox).filter(CryovialBox.id == cryo_id).first_or_404()
+    return "Hello World"
+
+@storage.route("/cryobox/view/LIMBCR-<cryo_id>/data")
+def view_cryobox_api(cryo_id):
+    cryo = db.session.query(CryovialBox).filter(CryovialBox.id == cryo_id).first_or_404()
+    return jsonify(cryo.data), 201, {"Content-Type": "application/json"}
