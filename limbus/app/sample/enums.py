73c428af78b1310f4147b248f30871286020afb3
KeironO <keiron.oshea@gmail.com>
2020-04-16 16:31:43 +0100

    [I] Base Enums for Forms

--- limbus/app/sample/enums.py
+++ limbus/app/sample/enums.py
@@ -8,6 +8,8 @@ class SampleType(FormEnum):
 
 
 class FluidSampleType(FormEnum):
+    # If Fluid the Quantity == ml
+
     ASC = "Ascites fluid"
     AMN = "Amniotic fluid"
     BAL = "Bronchoalveolar lavage"
@@ -15,11 +17,59 @@ class FluidSampleType(FormEnum):
     BMA = "Bone marrow aspirate"
     BMK = "Breast milk"
     BUC = "Buccal cells"
+    BILE = "Bile"
     BUF = "Unficolled buffy coat, viable"
     BFF = "Unficolled buffy coat, non-viable"
+    BCF = "Body Cavity Fluid"
+    BMP = "Bone Marrow Plasma"
+    CER = "Cerebrospinal Fluid"
+    FEC = "Feces"
+    GAF = "Gastric Fluid"
+    LAV = "Lavage"
+    MIL = "Milk"
     CEL = "Ficoll mononuclear cells, viable"
     CEN = "Fresh cells from non-blood specimen type"
+    PLA = "Plasma"
+    SAL = "Saliva"
+    SER = "Serum"
+    SPU = "Sputum"
+    SWE = "Sweat"
+    SYN = "Synovial Fluid"
+    URI = "Urine"
+    VIT = "Vitreous Fluid"
 
+
+class MolecularSampleType(FormEnum):
+    # If MolecularSample Quantity ==  uberg + concentration? ug/ml
+    CDN = "cDNA"
+    CTD = "ctDNA"
+    DNA = "DNA"
+    PRO = "Protein"
+    RNA = "RNA"
+    RNC = "RNA, cytoplasmic"
+    RNM = "RNA, nulcear"
+    RNP = "RNA, poly-A enriched"
+    TNA = "Total Nucleic Acid"
+    WGA = "Whole Genome Amplified DNA"
+
+class CellSampleType(FormEnum):
+    # If Cell Quantity == Cells
+    CYO = "Cyropreserved Cells"
+    FXB = "Fixed Cell Block"
+    FCB = "Frozen Cell Block"
+    FCP = "Frozen Cell Pellet"
+    PRM = "PMRC"
+    SLI = "Slide"
+
+class TissueSampleType(FormEnum):
+    FIX = "Fixed Tissue"
+    FTB = "Fixed Tissue Block"
+    FTS = "Fixed Tissue Slide"
+    FRE = "Fresh Tissue"
+    FRO = "Frozen Tissue"
+    FROTB = "Frozen Tissue Block"
+    FROTS = "Frozen Tissue Slide"
+    MDS = "Microdissected"
 
 class DisposalInstruction(FormEnum):
     DES = "Destroy"
