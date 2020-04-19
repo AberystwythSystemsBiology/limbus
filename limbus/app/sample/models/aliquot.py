159217715a312b3cb80a86e88d1bffed7c4cf56c
KeironO <keiron.oshea@gmail.com>
2020-04-19 06:52:25 +0100

    [I] I need to rejig the protocols

--- limbus/app/sample/models/aliquot.py
+++ limbus/app/sample/models/aliquot.py
@@ -5,7 +5,6 @@ class SubSampleToSample(db.Model):
     __tablename__ = "subsample_to_samples"
     id = db.Column(db.Integer, primary_key=True)
 
-    subsample_type = db.Column(db.Enum(SubSampleType))
     parent_sample = db.Column(db.Integer, db.ForeignKey("samples.id"))
 
     creation_date = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
--- limbus/app/sample/views/aliquot.py
+++ limbus/app/sample/views/aliquot.py
@@ -34,20 +34,58 @@ def aliquot(sample_id):
         size = form.size.data
         aliquot_date = form.aliquot_date.data
         aliquot_time = form.aliquot_time.data
+        selected_sample_type = form.sample_type.data
 
         lock_parent = form.lock_parent.data
 
         for i in range(counts):
             # Create Sample
-            a_s = Sample()
+            a_s = Sample(
+                sample_type = sample.sample_type,
+                creation_date = aliquot_date,
+                collection_date = aliquot_time,
+                quantity = size,
+                current_quantity = size,
+                disposal_date = sample.disposal_date,
+                author_id = current_user.id
+            )
+
+            db.session.add(a_s)
+            db.session.flush()
 
             # Sample Type
+            if sample.sample_type == SampleType.MOL:
+                s_sample_type = SampleToMolecularSampleType
+            elif sample.sample_type == SampleType.FLU:
+                s_sample_type = SampleToFluidSampleType
+            else:
+                s_sample_type = SampleToCellSampleType
+
+            sst = s_sample_type(
+                sample_id = a_s.id,
+                sample_type = selected_sample_type,
+                author_id = current_user.id
+            )
+
+            db.session.add(sst)
+            db.session.flush()
 
             # Add SubSample
-            s_ss = SubSampleToSample()
+            s_ss = SubSampleToSample(
+                parent_sample = sample.id,
+                author_id = current_user.id
+            )
 
-            # Update Sample to reflect quantity change.
-            #sample
+            db.session.add(s_ss)
+            db.session.commit()
+
+        sample.current_quantity = size * counts
+
+        if lock_parent:
+            sample.is_closed = True
+
+        db.session.add(sample)
+        db.session.commit()
 
 
 
