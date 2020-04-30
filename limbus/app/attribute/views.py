2ca14a49e86dc81fb0c236910b14dfaee2d765bb
KeironO <keiron.oshea@gmail.com>
2020-04-30 05:59:58 +0100

    [I] Added view for User

--- limbus/app/attribute/views.py
+++ limbus/app/attribute/views.py
@@ -1,41 +1,32 @@
 from .models import CustomAttributes
 from ..auth.models import User
 from .. import db
-from ..ViewClass import ViewClass
 
-
-class CustomAttributesIndexView(ViewClass):
+def CustomAttributesIndexView():
     """
-        This class returns an dictionary of information concerning attributes, as well as their author information.
-
-        It is used in the following routes:
-            - __init__.py :: index
-
-        It is used in the following apis:
-            - None.
+        This method returns an dictionary of information concerning attributes, as well as their author information.
     """
-    def __init__(self):
-        self.attributes = db.session.query(CustomAttributes, User).filter(CustomAttributes.author_id == User.id).all()
+    attributes = db.session.query(CustomAttributes, User).filter(CustomAttributes.author_id == User.id).all()
 
-    def get_attributes(self) -> dict:
-        data = {}
-        for attribute, user in self.attributes:
+    data = {}
 
-            data[attribute.id] = {
-                "term" : attribute.term,
-                "description" : attribute.description,
-                "type" : attribute.type.value,
-                "accession": attribute.accession,
-                "ref": attribute.ref,
-                "element": attribute.element.value,
-                "required": attribute.required,
-                "creation_date": attribute.creation_date,
-                "user_information" : {
-                    "email": user.email,
-                    "name": user.name,
-                    "gravatar": user.gravatar()
-                }
+    for attribute, user in attributes:
+
+        data[attribute.id] = {
+            "term" : attribute.term,
+            "description" : attribute.description,
+            "type" : attribute.type.value,
+            "accession": attribute.accession,
+            "ref": attribute.ref,
+            "element": attribute.element.value,
+            "required": attribute.required,
+            "creation_date": attribute.creation_date,
+            "user_information" : {
+                "email": user.email,
+                "name": user.name,
+                "gravatar": user.gravatar()
             }
+        }
 
         return data
 
---
+++ limbus/app/auth/routes.py
@@ -0,0 +1,46 @@
+from flask import redirect, render_template, url_for, flash
+from flask_login import login_required, login_user, logout_user, current_user
+
+from . import auth
+
+from .forms import LoginForm, ChangePassword
+from .models import User
+
+from .views import UserView
+
+from .. import db
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = db.session.query(User).filter(User.email == form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user)
+            return redirect(url_for("misc.index"))
+        else:
+            flash("Incorrect email or password.")
+    return render_template("auth/login.html", form=form)
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have successfully been logged out.")
+    return redirect(url_for("auth.login"))
+
+
+@auth.route("/profile", methods=["GET", "POST"])
+def profile():
+    user = UserView(current_user.id)
+
+    password_change = ChangePassword()
+
+    return render_template(
+        "auth/profile.html",
+        user=user,
+        password_change=password_change,
+    )
