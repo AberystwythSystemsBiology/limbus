b7c870bae609f9f85d62085fb5880baaaadec551
KeironO <keiron.oshea@gmail.com>
2020-05-06 14:37:00 +0100

    [I] Horrible hack

--- services/web/dumpdb.py
+++ services/web/dumpdb.py
@@ -4,10 +4,13 @@ import os
 from datetime import datetime
 import hashlib
 from pathlib import Path
+import glob
 
-dump_dir = os.environ["DUMP_FOLDER"]
+def write_json(result, fp):
+    with open(os.path.join(os.environ["DUMP_FOLDER"], fp), "w") as o:
+        json.dump(result, o, indent=4, default=str)
 
-Path(dump_dir).mkdir(exist_ok=True)
+Path(os.environ["DUMP_FOLDER"]).mkdir(exist_ok=True)
 
 engine = sqAl.create_engine("postgresql+psycopg2://{user}:{passwd}@db:5432/{db}".format(
     user=os.environ["POSTGRES_USER"],
@@ -23,9 +26,17 @@ for table in meta.sorted_tables:
 
 m = hashlib.new("ripemd160")
 m.update(str(result).encode('utf-8'))
+h = m.hexdigest()
 
-fp = "%s:%s" % (m.hexdigest(), datetime.now().strftime("%d-%m-%Y-%H-%M-%S"))
+fp = "%s:%s.json" % (h, datetime.now().strftime("%d-%m-%Y-%H-%M-%S"))
 
-print(fp)
+if os.listdir(os.environ["DUMP_FOLDER"]) != []:
+    glob_dir = os.path.join(os.environ["DUMP_FOLDER"], '*')
+    newest = max(glob.glob(glob_dir), key=os.path.getctime)
+    if newest.split(":")[0].split("/")[-1] != h:
+        write_json(result, fp)
+else:
+    write_json(result, fp)
 
-#print(json.dumps(result, indent=4, default=str))
+
+#print()
