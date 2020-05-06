9f74b09854f2bc378d7bf3586b3c7ca60cc61033
KeironO <keiron.oshea@gmail.com>
2020-05-06 11:20:09 +0100

    [H] uh?#

---
+++ services/web/instance/config.py
@@ -0,0 +1,10 @@
+import os
+
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{user}:{passwd}@postgres:5432/{db}".format(
+    user=os.environ["POSTGRES_USER"],
+    passwd=os.environ["POSTGRES_PASSWORD"],
+    db=os.environ["POSTGRES_DB"])
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+WTF_CSRF_SECRET_KEY = os.environ["WTF_CSRF_SECRET_KEY"]
+DOCUMENT_DIRECTORY = os.environ["DOCUMENT_DIRECTORY"]
