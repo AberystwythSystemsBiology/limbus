e19bf3a61c5c3435e2780a1050ee60cb1d7dfbea
KeironO <keiron.oshea@gmail.com>
2020-07-24 17:47:11 +0100

    Added theme and nicer name

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'The LIbre Biobank Management System'
+project = 'LImBuS'
 copyright = "2020, Keiron O'Shea"
 author = "Keiron O'Shea"
 
@@ -52,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'pyramid'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
