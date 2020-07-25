18e86fe170549a04e450797bca63d3e03440bad9
KeironO <keiron.oshea@gmail.com>
2020-07-25 10:53:22 +0100

    Added stuff to documentation

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_logo = 'img/logo.svg'
 html_theme_options = {
     'logo_only': True,
