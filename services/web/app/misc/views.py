990d1c9545e55af3a16873d6bd2341f5079f3090
KeironO <keiron.oshea@gmail.com>
2020-07-27 22:08:25 +0100

    Added schemas for Site and Address, but need to do validation and DB helper scripts

--- services/web/app/misc/views.py
+++ services/web/app/misc/views.py
@@ -15,3 +15,37 @@ class NewAddressSchema(masql.SQLAlchemySchema):
     county = masql.auto_field()
     post_code = masql.auto_field()
     country = masql.auto_field()
+    author_id = masql.auto_field()
+
+class NewSiteInformationSchema(masql.SQLAlchemySchema):
+    class Meta:
+        model = SiteInformation
+    
+    miabis_id = masql.auto_field()
+    acronym = masql.auto_field()
+    name = masql.auto_field()
+    description = masql.auto_field()
+    url = masql.auto_field()
+    author = masql.auto_field()
+
+class SiteInformationSchema(masql.SQLAlchemySchema):
+    class Meta:
+        model = SiteInformation
+    
+    miabis_id = masql.auto_field()
+    acronym = masql.auto_field()
+    name = masql.auto_field()
+    description = masql.auto_field()
+    url = masql.auto_field()
+
+class AddressSchema(masql.SQLAlchemySchema):
+    class Meta:
+        model = Address
+
+    street_address_one = masql.auto_field()
+    street_address_two = masql.auto_field()
+    city = masql.auto_field()
+    county = masql.auto_field()
+    post_code = masql.auto_field()
+    country = masql.auto_field()
+    author = masql.auto_field()
