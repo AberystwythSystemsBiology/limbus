3a14a601638d5667d68b5f6af6ecf1bf32dee81d
KeironO <keiron.oshea@gmail.com>
2020-07-28 11:28:02 +0100

    Problem

--- services/web/app/auth/views.py
+++ services/web/app/auth/views.py
@@ -31,6 +31,20 @@ class NewUserAccountSchema(masql.SQLAlchemySchema):
     password = fields.Str(required=True)
 
 
+new_user_account_schema = NewUserAccountSchema()
 
+class FullUserAccountSchema(masql.SQLAlchemySchema):
+    class Meta:
+        model = UserAccount
 
+    email = masql.auto_field()
+    title = masql.auto_field()
+    first_name = masql.auto_field()
+    middle_name = masql.auto_field()
+    last_name = masql.auto_field()
+
+    account_type = masql.auto_field()
+    access_control = masql.auto_field()
+
+full_user_account_schema = FullUserAccountSchema()
 full_user_accounts_schema = FullUserAccountSchema(many=True)
