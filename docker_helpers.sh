28903accd93cbb10922dc55df39bdb40524f2476
KeironO <keiron.oshea@gmail.com>
2020-08-25 11:26:46 +0100

    Cleaned up helper script

--- docker_helpers.sh
+++ docker_helpers.sh
@@ -1,65 +1,107 @@
 #!/bin/bash
 
-function limbus-pip-install() {
-    echo "Enter the pip package you want to install:"
-    read package
-    echo ">>> Now attempting to install $package:"
-    docker-compose run web sh -c "python3 -m venv venv && venv/bin/pip install $package"
-    echo ">>> Writing to requirements.txt:"
-    docker-compose run web sh -c "python3 -m venv venv && venv/bin/pip freeze > requirements.txt"
-}
-
-function limbus-c() {
-    echo ">>>> Cleaning binary files"
-    docker-compose run web sh -c "find . -type f -name '*.pyc' -exec rm {} +"
-}
-
-function limbus-create-kryten() {
+function limbus_create_kryten() {
     echo ">>>> Creating Kryten"
     docker-compose run web sh -c "flask cmd_setup create-kryten"
 }
 
-function limbus-create-testing-user() {
-   limbus-create-kryten()
-   docker-compose run web sh -c "flask cmd_setup create-testuser"
+# This function allows the user to install a python package via 
+# the pypi repository.
+function limbus_pip_install() {
+    echo "Enter the pip package you want to install:"
+    read package
+    echo "  >>> Now attempting to install $package:"
+    docker-compose run web sh -c "pip3 install $package"
+
+    echo "Do you want to save the changes to requirements.txt? (Y/N)"
+    read answer
+
+    if [ ${answer^^} == "Y" ]; then
+        echo "  >>> Writing to requirements.txt:"
+        docker-compose run web sh -c "pip3 freeze > requirements.txt"
+    fi
 }
 
-function limbus-bwd() {
-    limbus-b
-    python-deps
-    yarn-deps
+function limbus_clear_binary_files() {
+    echo "  >>> Cleaning binary files"
+    docker-compose run web sh -c "find . -type f -name '*.pyc' -exec rm {} +"
 }
 
-function python-deps() {
-    echo ">>> Setting up Python dependencies:"
-    docker-compose run web sh -c "venv/bin/python -m venv venv ; venv/bin/python -m pip install -r requirements.txt"
-}
-
-function yarn-deps() {
+function limbus_yarn_install() {
     echo ">>> Running yarn install:"
     docker-compose run web sh -c "yarn install"
 }
 
-function limbus-b() {
-    echo ">>>> Building from Dockerfile"
+
+function limbus_db_nuke() {
+    echo "WARNING: THIS WILL DESTROY THE DATABASE, INCLUDING VERSIONS"
+
+    echo "Are you sure you want todo this? (Y/N)"
+    read answer
+    if [ ${answer^^} == "Y" ]; then
+        docker-compose run web sh -c "alembic stamp base"
+        docker-compose run web sh -c "rm migrations/versions/*"
+        docker-compose down -v
+    fi
+}
+
+function limbus_db_create() {
+    echo "CREATING DATABASE"
+
+    if [ ! -d ./services/web/migrations/versions ]; then
+        mkdir -p ./services/web/migrations/versions
+    fi
+
+    docker-compose run web sh -c "alembic revision --autogenerate -m 'Generating database'; alembic upgrade head"
+}
+
+function limbus_db_upgrade() {
+    echo "Preparing datbase update"
+    # A lil bit of fun 
+    echo "alembic commit -m"
+    read commit
+    docker-compose run web sh -c "alembic revision --autogenerate -m '$commit'"
+    docker-compose run web sh -c "alembic upgrade head"
+}
+
+function limbus_test_entrypoint() {
     docker-compose build
+    limbus_yarn_install
+    docker-compose run --service-ports web sh -c "python3 limbus/test_basic.py"
 }
 
-function limbus-s() {
-    echo ">>>> Starting limbus daemon, to close it run limbus-d: to check logs run limbus-logs"
-    docker-compose up -d
-}
+function limbus_setup_dev() {
+    echo "WELCOME TO LIMBUS"
+    echo "================="
 
-function limbus-logs() {
-    docker-compose logs -f    
-}
+    echo "Do you want to build from the Dockerfile? (Y/N)"
+    read answer_one
 
-function limbus-d() {
-    echo ">>>> Closing limbus daemon"
-    docker-compose down
-}
+    if [ ${answer_one^^} == "Y" ]; then
+        echo ">>>> Building from Dockerfile"
+        docker-compose build
+    fi
+    
+    echo "Do you want to install yarn dependencies? (Y/N)"
+    read answer_two
 
-function limbus-test() {
-    docker-compose run --service-ports web sh -c "venv/bin/python limbus/test_basic.py"
-    limbus-c
-}
+    if [ ${answer_two^^} == "Y" ]; then
+        limbus_yarn_install
+    fi
+    
+    echo "Do you want to set up the database? (Y/N)"
+    read answer_three
+
+    if [ ${answer_three^^} == "Y" ]; then
+        limbus_db_nuke
+        limbus_db_create
+    fi
+
+    echo "Creating Kryten"
+    limbus_create_kryten
+
+    echo "You should now be able to start up your dev environment by running:"
+    echo "$ docker-compose up"
+
+    echo "Happy hacking :)"
+}  
