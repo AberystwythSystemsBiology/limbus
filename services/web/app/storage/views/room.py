29f9bedb64d34c5d29795e89075d58a85b28f38a
KeironO <keiron.oshea@gmail.com>
2020-08-27 16:48:36 +0100

    Removed old views

--- services/web/app/storage/views/room.py
+++ services/web/app/storage/views/room.py
@@ -13,33 +13,3 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from ... import db
-from ..models.room import Room
-from ..models.lts import FixedColdStorage
-from ...auth.views import UserView
-from .lts import BasicLTSView
-
-
-def BasicRoomView(room_id: int) -> dict:
-    r = db.session.query(Room).filter(Room.id == room_id).first_or_404()
-    return {
-        "id": r.id,
-        "room_number": r.room_number,
-        "building": r.building,
-        "creation_date": r.creation_date,
-        "site_id": r.site_id,
-        "author_information": UserView(r.author_id),
-    }
-
-
-def RoomView(room_id: int) -> dict:
-    data = BasicRoomView(room_id)
-
-    data["storage"] = [
-        BasicLTSView(x.id)
-        for x in db.session.query(FixedColdStorage)
-        .filter(FixedColdStorage.room_id == room_id)
-        .all()
-    ]
-
-    return data
