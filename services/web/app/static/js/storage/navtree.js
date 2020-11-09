3cb2373658b92089df1cdfbdf93e726f61f221da
KeironO <keiron.oshea@gmail.com>
2020-11-09 01:28:16 +0000

    net is kil

--- services/web/app/static/js/storage/navtree.js
+++ services/web/app/static/js/storage/navtree.js
@@ -1,49 +1,14 @@
+
 function sap2tree(sap) {
-    var sites = sap['sites'];
-    for (var i = 0; i < sites.length; i++) {
-        var site = sites[i];
-        site['text'] = site['name'];
-        site['type'] = 'site';
-        site['children'] = site['rooms'];
-        site['id'] = `site:${site["id"]}`
-        for (var j = 0; j < site['rooms'].length; j++) {
-            var room = site['rooms'][j];
-            room['text'] = `${room['building']}: ${room['number']}`;
-            room['type'] = 'room';
-            room['children'] = room['fridges'];
-            room['id'] = `room:${room["id"]}`
-            for (var k = 0; k < room['fridges'].length; k++) {
-                var fridge = room['fridges'][k];
-                fridge['text'] = `${fridge['manufacturer']}: ${fridge['serial']}`;
-                fridge['type'] = 'fridge';
-                fridge['children'] = fridge['shelves'];
-                fridge['id'] = `fridge:${fridge["id"]}`
-                for (var l = 0; l < fridge['shelves'].length; l++) {
-                    var shelf = fridge['shelves'][l];
-                    shelf['text'] = shelf['name'];
-                    shelf['type'] = 'shelf';
-                    shelf['children'] = shelf['cryoboxes'].concat(shelf['samples']);
-                    shelf['id'] = `shelf:${shelf["id"]}`
-                    for (var m = 0; m < shelf['cryoboxes'].length; m++) {
-                        var box = shelf['cryoboxes'][m];
-                        box['text'] = `Box ${box["id"]}`;
-                        box['type'] = 'box';
-                        box['id'] = `box:${box["id"]}`
-                    }
-                    for (var m = 0; m < shelf['samples'].length; m++) {
-                        var sample = shelf['samples'][m];
-                        sample['text'] = `Sample ${sample['id']}`;
-                        sample['type'] = 'sample';
-                        sample['id'] = `sample:${sample["id"]}`
-                    }
-                }
-            }
-        }
-    }
+    var sites = sap["content"];
+
+
+
 
     return {
         'types': {
             'home': { 'icon': 'fa fa-home' },
+            'building': { 'icon': 'fa fa-home'},
             'site': { 'icon': 'fa fa-hospital' },
             'room': { 'icon': 'fa fa-door-closed' },
             'fridge': { 'icon': 'fa fa-temperature-low' },
@@ -63,6 +28,7 @@ function sap2tree(sap) {
     }
 }
 $(function() {
+
     $('#sidebar-collapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('#sidebar-collapse-icon').toggleClass('fa-chevron-left');
