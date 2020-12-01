8d99f63c0a00562ab492832a718600b45e071080
KeironO <keiron.oshea@gmail.com>
2020-12-01 13:02:54 +0000

    Added copy to clipboard

---
+++ services/web/app/static/js/auth/token.js
@@ -0,0 +1,12 @@
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+  
+
+$(document).ready(function(){ 
+    
+});
