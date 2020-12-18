8191cf8764a937f8d0172a89276b5fff5e1ba921
KeironO <keiron.oshea@gmail.com>
2020-12-18 15:46:45 +0000

    Aah

--- services/web/app/static/js/chart.js
+++ services/web/app/static/js/chart.js
@@ -46,6 +46,8 @@ function make_pie(dom_id, data, labels) {
 }
 
 function make_bar(dom_id, data, labels) {
+
+
     new Chart(document.getElementById(dom_id), {
         type: 'bar',
         data: {
@@ -63,6 +65,24 @@ function make_bar(dom_id, data, labels) {
             },
             title: {
                 display: false
-               }}}
+               },
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        beginAtZero: true,
+                        userCallback: function(label, index, labels) {
+                            // when the floored value is the same as the value we have a whole number
+                            if (Math.floor(label) === label) {
+                                return label;
+                            }
+       
+                        },
+                        max: Math.floor(Math.max(...data) * 1.3),
+                        stepValue: 1,
+                    }
+                    }
+                ]
+            }}}
         );
 }
