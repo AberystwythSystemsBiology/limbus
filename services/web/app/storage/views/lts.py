458bf5d5db028f848dd20c6deed91c1a49a03f51
KeironO <keiron.oshea@gmail.com>
2021-01-03 15:56:13 +0000

    Cleaned up js

--- services/web/app/storage/views/lts.py
+++ services/web/app/storage/views/lts.py
@@ -32,6 +32,7 @@ class ColdStorageServiceSchema(masql.SQLAlchemySchema):
     status = EnumField(ColdStorageServiceResult, by_value=True)
     comments = masql.auto_field()
     temp = masql.auto_field()
+    author = ma.Nested(BasicUserAccountSchema)
 
 cold_storage_service_schema = ColdStorageServiceSchema()
 cold_storage_services_schema = ColdStorageServiceSchema(many=True)
