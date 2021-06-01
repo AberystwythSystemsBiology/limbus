9fe9bff625b2c533b7ccd9c2fcbb25f4c759f110
KeironO <keiron.oshea@gmail.com>
2021-06-01 15:39:37 +0100

    Base model

---
+++ services/web/app/event/models.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2019  Keiron O'Shea <keo7@aber.ac.uk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from ..database import db, Base
+from .enums import DocumentType
+from ..mixins import RefAuthorMixin, RefEditorMixin
+
+
+class Event(Base, RefAuthorMixin, RefEditorMixin):
+    __versioned__ = {}
+    
+    datetime = db.Column(db.DateTime, nullable=False)
+    undertaken_by = db.Column(db.String(128))
+    comments = db.Column(db.Text())
