3b6f5a80fdb45743d24a7e7dda036dfe5429c26c
KeironO <keiron.oshea@gmail.com>
2022-02-05 09:59:52 +0000

    fixup: Format Python code with Black

--- services/web/app/commands/setup.py
+++ services/web/app/commands/setup.py
@@ -22,6 +22,7 @@ from uuid import uuid4
 import random
 import string
 
+
 @cmd_setup.cli.command("create-kryten")
 def create_kryten():
     kryten_ascii = """             _--~~--_
@@ -106,7 +107,7 @@ def create_testuser():
     db.session.commit()
     db.session.flush()
 
-    generated_password = ''.join(random.choice(string.printable) for i in range(15))
+    generated_password = "".join(random.choice(string.printable) for i in range(15))
 
     me = UserAccount(
         email="me@domain.com",
@@ -127,4 +128,7 @@ def create_testuser():
     db.session.commit()
 
     print("Created the testing/development user.")
-    print("You can log in using the following credentials:\nEmail: me@domain.com\nPassword: %s" % (generated_password))
+    print(
+        "You can log in using the following credentials:\nEmail: me@domain.com\nPassword: %s"
+        % (generated_password)
+    )
